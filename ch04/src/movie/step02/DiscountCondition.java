ea4d9d4e283042bc16e2779615e86f9d9e36f21e
ckstjr123 <a01027708644@gmail.com>
2026-01-04 01:15:13 +0900

    ch04_설계 품질과 트레이드오프

--- ch04/src/movie/step02/DiscountCondition.java
+++ ch04/src/movie/step02/DiscountCondition.java
@@ -4,10 +4,9 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class DiscountCondition {
+
     private DiscountConditionType type;
-
     private int sequence;
-
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
     private LocalTime endTime;
